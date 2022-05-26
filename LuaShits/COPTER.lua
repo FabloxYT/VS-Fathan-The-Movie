d2c4a9511334d22ca15ca05415c0f74dbc1795fd
FabloxYT <106307504+FabloxYT@users.noreply.github.com>
2022-05-26 16:26:31 +0700

    HELICOPTER
    AAA PARGOY

---
+++ LuaShits/COPTER.lua
@@ -0,0 +1,31 @@
+local canattack = false;
+local mecha = false
+local attacked = false;
+local idlebfthing = false
+---
+function onCreatePost() 
+    makeLuaText("typeText", "Song Type: NIGHTMARE", 500, 30, 10)
+    setTextAlignment("typeText", "left")
+    addLuaText("typeText")
+
+        makeLuaText("creatorText", "Created by GibranTheGamerCool & Jazz", 500, 30, 50)
+        setTextAlignment("creatorText", "left")
+        addLuaText("creatorText")
+
+        makeLuaText("songText", "Accidental", 500, 30, 30)
+        setTextAlignment("songText", "left")
+        addLuaText("songText")
+
+    if getPropertyFromClass('ClientPrefs', 'downScroll') == false then
+        setProperty('typeText.y', 640)
+        setProperty('creatorText.y', 680)
+        setProperty('songText.y', 660)
+    end
+end
+
+function opponentNoteHit(id, d, noteType, isSustainNote)
+	if not lowQuality then
+	cameraShake('game', 0.007, 0.1)
+    cameraShake('hud', 0.007, 0.1)
+	end
+end
