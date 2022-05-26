f5edc7c54b7382e5511ef677e7eb03026180e412
FabloxYT <106307504+FabloxYT@users.noreply.github.com>
2022-05-26 16:30:13 +0700

    T
    T

---
+++ LuaShits/Time.lua
@@ -0,0 +1,29 @@
+function onCreatePost() 
+    makeLuaText("songText", "Breakout", 500, 30, 50)
+    setTextAlignment("songText", "left")
+    addLuaText("songText")
+
+    makeLuaText("creatorText", "Created by FabloxYT & GREYISSUS", 500, 30, 30)
+    setTextAlignment("creatorText", "left")
+    addLuaText("creatorText")
+
+    makeLuaText("typeText", "Song Type: Story Songs", 500, 30, 10)
+    setTextAlignment("typeText", "left")
+    addLuaText("typeText")
+
+    if getPropertyFromClass('ClientPrefs', 'downScroll') == false then
+        setProperty('songText.y', 660)
+        setProperty('typeText.y', 640)
+        setProperty('creatorText.y', 680)
+    end
+end
+
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+		startVideo('week1/3');
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
